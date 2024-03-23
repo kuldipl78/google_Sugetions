4d9dab370923ce0e01d4856d178f7dd58627e6c3
kuldip lohare <162694586+kuldipl78@users.noreply.github.com>
2024-03-23 18:13:13 +0530

    Update index.js

--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,1 +1,18 @@
-// Write your code here
+import React from 'react'
+
+import './index.css'
+
+const SuggestionItem = ({onSuggetionClick, suggestion}) => {
+  return (
+    <li className="list-Container">
+      <button
+        className="btn-element"
+        onClick={() => onSuggetionClick(suggestion)}
+      >
+        {suggestion}
+      </button>
+    </li>
+  )
+}
+
+export default SuggestionItem
